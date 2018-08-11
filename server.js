9555b050b5e1d6fcf99eee177ec14212ac8ca804
Lindsey Bowen <lindsbow102@yahoo.com>
2018-08-11 13:32:26 -0700

    Render handlebars templates with data

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
-    //res.send('<h1>Hello Express!</h1>');
-    res.send({
-        name: 'Lindsey',
-        age: 38,
-        hobbies: ['running', 'netflixing', 'coding']
-    });
+    res.render('home.hbs', {
+        pageTitle: 'Home Page', 
+        welcomeMessage: 'Welcome to my site!  Glad to see you.',
+        currentYear: new Date().getFullYear()
+    })
 });
 
 app.get('/about', (req, res) => {
-    res.send('About Page');
+    res.render('about.hbs', {
+        pageTitle: 'About Page', 
+        currentYear: new Date().getFullYear()
+    }); // From Views folder
 });
 
 app.get('/bad', (req, res) => {
@@ -21,4 +27,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('Server is up on port 3000!');
+});
